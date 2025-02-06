2ce466bbc12bc39cbd86fd3cf206d0c93eae0796
Tatianos7 <balan.tv.20.05@gmail.com>
2025-02-06 17:15:17 +0300

    Меняет наставника

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 
 import EventPresenter from './presenter/event-presenter.js';
 import HeaderPresenter from './presenter/header-presenter.js';
+import PointsModel from './model/point-model.js';
 
 const siteMainElement = document.querySelector('.trip-events');
 const siteHeaderElement = document.querySelector('.trip-main');
 
-const eventPresenter = new EventPresenter({container: siteMainElement});
+const pointModel = new PointsModel();
+const eventPresenter = new EventPresenter({container: siteMainElement, pointModel});
 const headerPresenter = new HeaderPresenter({container: siteHeaderElement});
 
 headerPresenter.init();
