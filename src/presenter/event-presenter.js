c3cbe20fe2502e6d71aa14bf1aff2a58e7305c7e
Tatianos7 <balan.tv.20.05@gmail.com>
2025-02-17 15:59:06 +0300

    Реализует смену карточки на форму

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -2,10 +2,10 @@ import SortView from '../view/sort-view.js';
 import ListView from '../view/list.view.js';
 import PointView from '../view/point-view.js';
 import EditPointFormView from '../view/edit-point-form-view.js';
-import { render } from '../framework/render.js';
+import { render, replace } from '../framework/render.js';
 export default class EventPresenter {
-  #boardContainer = null;
-  #pointsModel = null;
+  #boardContainer;
+  #pointsModel;
 
   #listOfTrips = new ListView();
 
@@ -20,21 +20,45 @@ export default class EventPresenter {
     this.#tripPoints = [...this.#pointsModel.points];
     render(new SortView(), this.#boardContainer);
     render(this.#listOfTrips, this.#boardContainer);
-    render(new EditPointFormView ({
-      point: this.#tripPoints[0],
-      checkedOffers: [...this.#pointsModel.getOffersByTypeAndIds(this.#tripPoints[0].type, this.#tripPoints[0].offers)],
-      offers: this.#pointsModel.getOfferByType(this.#tripPoints[0].type),
-      allDestinations: this.#pointsModel.getDestination(this.#tripPoints[0].destination),
-      destination: this.#pointsModel.getDestinationId(this.#tripPoints[0].destination)
-    }), this.#listOfTrips.element);
-
-    for (let i = 1; i < this.#tripPoints.length; i++) {
-      render(new PointView({
-        point: this.#tripPoints[i],
-        offers: [...this.#pointsModel.getOffersByTypeAndIds(this.#tripPoints[i].type, this.#tripPoints[i].offers)],
-        destination: this.#pointsModel.getDestinationId(this.#tripPoints[i].destination)
-      }), this.#listOfTrips.element);
+    for (let i = 0; i < this.#tripPoints.length; i++) {
+      this.#renderPoint(this.#tripPoints[i]);
     }
+  }
+  #renderPoint(point) {
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+    const pointComponent = new PointView({
+      point: point,
+      offers: [...this.#pointsModel.getOffersByTypeAndIds(point.type, point.offers)],
+      destination: this.#pointsModel.getDestinationId(point.destination),
+      onEditClick: () => {
+        replaceCardToForm();
+        document.addEventListener('keydown', escKeyDownHandler);
+      }
+    });
+    const pointEditComponent = new EditPointFormView({
+      point: point,
+      checkedOffers: [...this.#pointsModel.getOffersByTypeAndIds(point.type, point.offers)],
+      offers: this.#pointsModel.getOfferByType(point.type),
+      allDestinations: this.#pointsModel.getDestination(point.destination),
+      destination: this.#pointsModel.getDestinationId(point.destination),
+      onFormSubmit: () => {
+        replaceFormToCard();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    });
+    function replaceCardToForm() {
+      replace(pointEditComponent, pointComponent);
+    }
+    function replaceFormToCard() {
+      replace(pointComponent, pointEditComponent);
+    }
+    render(pointComponent, this.#listOfTrips.element);
   }
 }
 
