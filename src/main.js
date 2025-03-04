661ac75243ccb4df2a5be92b5f1e3a716c0441ea
Tatianos7 <balan.tv.20.05@gmail.com>
2025-03-04 15:05:29 +0300

    Устраняет ошибку

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const siteMainElement = document.querySelector('.trip-events');
 const siteHeaderElement = document.querySelector('.trip-main');
 
 const pointsModel = new PointsModel();
-const eventPresenter = new EventPresenter({container: pointsModel});
+const eventPresenter = new EventPresenter({container: siteMainElement, pointsModel});
 const headerPresenter = new HeaderPresenter({container: siteHeaderElement, pointsModel});
 
 headerPresenter.init();
